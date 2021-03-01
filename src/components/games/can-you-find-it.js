cfa6fa402d1b3c794c394b39fdb7a8413d48f255
Alexander Guevara <alexgue26@hotmail.com>
2021-03-01 01:27:19 -0500

    Added firebase and access to questions

--- src/components/games/can-you-find-it.js
+++ src/components/games/can-you-find-it.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+import firebase from "firebase/app";
+import 'firebase/database';
 
-function cyfi(){
-
-return(
-    <div style={{color:"red"}}>Can You Find It Game</div>
-)
+function Cyfi(){
+    const database = firebase.database()
+    // var ref = database.ref(`games/cyfi/Questions/0`);
+    // ref.on("value", function(snapshot) {
+    //     console.log(snapshot.val());
+    //  }, function (error) {
+    //     console.log("Error: " + error.code);
+    // });
+    function randomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    const [question, setQuestion] = useState("");
+    useEffect(()=>{
+        let ref= database.ref(`games/cyfi/Questions/${randomInt(2)}`)
+        ref.on("value", snapshot => {
+            setQuestion(snapshot.val())  
+         }, error => {
+            console.log("Error: " + error.code);
+        });
+    },[])
+    return(
+        <div>
+            
+            <h1>Can You Find It Game</h1>
+            <div>
+            <p>{question}</p>
+            </div>
+        
+        </div>
+        
+    )
     
 
 }
 
-export default cyfi;
+export default Cyfi;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,27 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Game from "./components/games/can-you-find-it";
+import firebase from "firebase/app";
+// import "firebase/realtimedatabase";
 
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+  apiKey: "apiKey",
+  authDomain: "stag-game-of-games.firebaseapp.com",
+  // For databases not in the us-central1 location, databaseURL will be of the
+  // form https://[databaseName].[region].firebasedatabase.app.
+  // For example, https://your-database-123.europe-west1.firebasedatabase.app
+  databaseURL: "https://stag-game-of-games-default-rtdb.firebaseio.com/",
+  storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+// var database = firebase.database();
 
 ReactDOM.render(
+  
   (<BrowserRouter>
     <Switch>
          <Route exact path="/" component={App}/>
