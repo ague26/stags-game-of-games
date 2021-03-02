8a092c8cb8ca22952595c9f0d8ee27fd57662c7f
Alexander Guevara <alexgue26@hotmail.com>
2021-03-01 21:13:13 -0500

    made an reveal answer button

--- src/components/games/can-you-find-it.js
+++ src/components/games/can-you-find-it.js
@@ -10,24 +10,56 @@ function Cyfi(){
     //  }, function (error) {
     //     console.log("Error: " + error.code);
     // });
-    function randomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+    function randomInt() {
+        let max = 2;
+        setrandomNumber(Math.floor(Math.random() * Math.floor(max)));
+    }
+    const revealButton = () =>{
+        setrevealAnswer(true)
     }
     const [question, setQuestion] = useState("");
+    const [revealAnswer, setrevealAnswer] = useState(false)
+    const [answer,setanswer] = useState("")
+    const [randomnumber, setrandomNumber]= useState("")
     useEffect(()=>{
-        let ref= database.ref(`games/cyfi/Questions/${randomInt(2)}`)
-        ref.on("value", snapshot => {
+        randomInt()
+        let refQuestions= database.ref(`games/cyfi/Questions/${randomnumber}`)
+        let refAnswers= database.ref(`games/cyfi/Answers/${randomnumber}`)
+        refQuestions.on("value", snapshot => {
             setQuestion(snapshot.val())  
          }, error => {
             console.log("Error: " + error.code);
         });
-    },[])
+        refAnswers.on("value", snapshot => {
+            setanswer(snapshot.val())  
+         }, error => {
+            console.log("Error: " + error.code);
+        });
+    },)
+    // useEffect(()=>{
+    //     let ref= database.ref(`games/cyfi/Questions/${randomInt(2)}`)
+    //     ref.on("value", snapshot => {
+    //         setQuestion(snapshot.val())  
+    //      }, error => {
+    //         console.log("Error: " + error.code);
+    //     });
+    // },[])
     return(
         <div>
             
             <h1>Can You Find It Game</h1>
             <div>
-            <p>{question}</p>
+                <p>{question}</p>
+            </div>
+            <div>
+                <button onClick={revealButton}>Reveal Answer</button>
+                {
+                    (revealAnswer === true)?
+                        answer
+                        :
+                        null
+                    
+                }
             </div>
         
         </div>
