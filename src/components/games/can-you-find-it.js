c65d88f3cf706c1f9330b5d00abc6d1b8b175af6
Alexander Guevara <alexgue26@hotmail.com>
2021-03-04 11:59:17 -0500

    Async/Await on firebase database

--- src/components/games/can-you-find-it.js
+++ src/components/games/can-you-find-it.js
@@ -4,46 +4,34 @@ import 'firebase/database';
 
 function Cyfi(){
   const database = firebase.database();
-  // var ref = database.ref(`games/cyfi/Questions/0`);
-  // ref.on("value", function(snapshot) {
-  //     console.log(snapshot.val());
-  //  }, function (error) {
-  //     console.log("Error: " + error.code);
-  // });
-  function randomInt() {
-    let max = 2;
-    setrandomNumber(Math.floor(Math.random() * Math.floor(max)));
-  }
-  const revealButton = () => {
-    setrevealAnswer(true)
+    
+  function revealButton() {
+    setrevealAnswer(true);
   }
   const [ question, setQuestion ] = useState("");
   const [ revealAnswer, setrevealAnswer ] = useState(false)
   const [ answer,setanswer ] = useState("")
-  const [ randomnumber, setrandomNumber ]= useState("")
+
   useEffect(() => {
-    randomInt();
-    let refQuestions = database.ref(`games/cyfi/Questions/${randomnumber}`);
-    let refAnswers = database.ref(`games/cyfi/Answers/${randomnumber}`);
-    refQuestions.on("value", snapshot => {
-      setQuestion(snapshot.val());
-    }, error => {
-      console.log("Error: " + error.code);
-    });
-    refAnswers.on("value", snapshot => {
-      setanswer(snapshot.val());
-    }, error => {
-      console.log("Error: " + error.code);
-    });
-  },)
-  // useEffect(()=>{
-  //     let ref= database.ref(`games/cyfi/Questions/${randomInt(2)}`)
-  //     ref.on("value", snapshot => {
-  //         setQuestion(snapshot.val())  
-  //      }, error => {
-  //         console.log("Error: " + error.code);
-  //     });
-  // },[])
+    const runDatabase = async()=>{
+      let refQuestionsObject = database.ref(`games/cyfi/Questions`);
+      const snapshotQuestionObject = await refQuestionsObject.once("value")
+      let numChildren = snapshotQuestionObject.numChildren()
+      let randomNumber =  Math.floor(Math.random() * Math.floor(numChildren));
+
+      let refQuestions = database.ref(`games/cyfi/Questions/${randomNumber}`);
+      const snapshotrefQuestions = await refQuestions.once("value");
+      let user = (snapshotrefQuestions.val())
+      console.log(user)
+      setQuestion(user)
+
+      let refAnswers = database.ref(`games/cyfi/Answers/${randomNumber}`);
+      const snapshotrefAnswers = await refAnswers.once("value");
+      let answer = (snapshotrefAnswers.val())
+      setanswer(answer)
+    }
+    runDatabase()
+  },[])
   return (
     <div>
       <h1>Can You Find It Game</h1>
